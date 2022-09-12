3a74fbec211294d69c438cb41e9432aac7ff32fd
mojo-kun <mosesmaina1245@gmail.com>
2022-09-12 13:45:41 -0700

    task2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - print lowercase alphabets
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	char c;
+
+	for (ch = 'a' ; ch <= 'z' ; ch++)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
