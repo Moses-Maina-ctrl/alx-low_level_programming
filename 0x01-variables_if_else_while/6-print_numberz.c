957422a396411c57257319e059f209efac622a93
mojo-kun <mosesmaina1245@gmail.com>
2022-09-12 14:25:40 -0700

    taskss6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - numbers single
+ *
+ * Return: o
+ */
+int main(void)
+{	int n;
+
+	for (n = 0 ; n < 10 ; n++)
+	{
+		putchar(c + '0');
+	}
+	putchar('\n');
+	return (0);
+}
