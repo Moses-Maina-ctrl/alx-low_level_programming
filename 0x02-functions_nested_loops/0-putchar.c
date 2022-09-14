c727aa5698a07ad06f3463547c2eb3f31b8fe605
mojo-kun <mosesmaina1245@gmail.com>
2022-09-14 08:54:43 -0700

    ts

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,7 +1,8 @@
 #include "main.h"
 /**
- * _putchar - writes the character c to stdout
- * @c: The character to print
+ * main - Entry point
+ *
+ * Description: 'the program's description'
  *
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
